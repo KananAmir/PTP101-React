2f7b498257218aacb2fe2e9103cdb8547951354b
KananAmir <kanan.amirov@code.edu.az>
2026-01-26 13:54:16 +0400

    react router dom intro

---
+++ 08.react-routing/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './layout/Footer'
+import Header from './layout/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/contact' element={<Contact />}/>
+    </Routes>
+
+    <Footer />
+    </>
+  )
+}
+
+export default App
